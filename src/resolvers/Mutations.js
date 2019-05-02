e71a2e74b58397150ac77e288ca68c788b35184e
alchematrix0 <alchematrix0@gmail.com>
2019-05-02 12:34:38 -0700

    add some demo resolvers and comments

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -1,3 +1,15 @@
 const Mutations = {
+  createMission(parent, args, ctx, info) {
+    // create a dog
+    console.dir(args)
+  }
 }
 module.exports = Mutations
+
+// an example Mutation in playground
+
+// mutation createMission {
+// 	createMission(title:"Visit the fair") {
+//     title
+//   }
+// }
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,19 @@
 const Query = {
+  // parent GraphQl query
+  // args passed to this Query
+  // ctx: context, headers, cookies
+  // info: details pertaining to the query
+  missions(parent, args, ctx, info) {
+    // dummy data, will eventually pull from the db itself
+    return [{title: 'Catch a wild Pikachu'}, {title: 'Scale the Eiffel tower'}]
+  }
 }
 module.exports = Query
+
+// an example Query for use in Playground
+
+// query allMissions {
+//   missions {
+//     title
+//   }
+// }
