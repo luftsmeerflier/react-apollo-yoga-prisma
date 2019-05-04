c790ae87f222599eb5a289e83d0ac5f04f3d2351
alchematrix0 <alchematrix0@gmail.com>
2019-05-04 15:54:58 -0700

    remove commented out section of render method in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,13 +24,7 @@ const ApolloApp = AppComponent => (
 );
 
 render(ApolloApp(App), document.getElementById('root'));
-//
-// render((
-//   <ApolloProvider client={client}>
-//     <App />
-//   </ApolloProvider>
-// ), document.getElementById('root'));
-//
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
